f6c4d91e45e58a234c86ad2fd83f9d351967fba9
Michal <michal.p.zarzycki@wp.pl>
2020-07-07 12:01:16 -0700

    Added background, logos and fill the teamBAr

--- script.js
+++ script.js
@@ -26,6 +26,62 @@ const clubs = {
     betis: {
         stadium:'./stadiums/betis.jpg',
         logo:'/logos/betis.png'
+    },
+    alaves: {
+        stadium:'./stadiums/alaves.jpg',
+        logo:'/logos/alaves.png'
+    },
+    atletico: {
+        stadium:'./stadiums/atletico.jpg',
+        logo:'/logos/atletico.png'
+    },
+    bilbao: {
+        stadium:'./stadiums/bilbao.jpg',
+        logo:'/logos/bilbao.png'
+    },
+    celta: {
+        stadium:'./stadiums/celta.jpg',
+        logo:'/logos/celta.png'
+    },
+    eibar: {
+        stadium:'./stadiums/eibar.jpg',
+        logo:'/logos/eibar.png'
+    },
+    espanol: {
+        stadium:'./stadiums/espanol.jpg',
+        logo:'/logos/espanol.png'
+    },
+    getafe: {
+        stadium:'./stadiums/getafe.jpg',
+        logo:'/logos/getafe.png'
+    },
+    granada: {
+        stadium:'./stadiums/granada.jpg',
+        logo:'/logos/granada.png'
+    },
+    leganes: {
+        stadium:'./stadiums/leganes.jpg',
+        logo:'/logos/leganes.png'
+    },
+    levante: {
+        stadium:'./stadiums/levante.jpg',
+        logo:'/logos/levante.png'
+    },
+    mallorca: {
+        stadium:'./stadiums/mallorca.jpg',
+        logo:'/logos/mallorca.png'
+    },
+    osasuna: {
+        stadium:'./stadiums/osasuna.jpg',
+        logo:'/logos/osasuna.png'
+    },
+    sociedad: {
+        stadium:'./stadiums/sociedad.jpg',
+        logo:'/logos/sociedad.png'
+    },
+    valladolid: {
+        stadium:'./stadiums/valladolid.jpg',
+        logo:'/logos/valladolid.png'
     }
 }
 
@@ -34,6 +90,9 @@ let logos = []
 for(key in clubs) {
     stadiums.push(clubs[key].stadium)
     logos.push(clubs[key].logo)
+}
+for(let i=0; i<logos.length; i++) {
+    document.querySelector(`.teamBox:nth-child(${i+1})`).style.backgroundImage = `url(${logos[i]})`
 }
 console.log(stadiums, logos)
 RIGHT_ARROW.addEventListener('click', ()=> {
