60df80bb00bceb98ace9fe47e147d4bcb9360ae2
Michal <michal.p.zarzycki@wp.pl>
2020-07-08 05:31:57 -0700

    Added dynamic background to main

--- script.js
+++ script.js
@@ -8,140 +8,179 @@ const clubs = {
         logo: '/logos/sevilla.png',
         font:'barcelona',
         navColors: ['#ffffff','#43333'],
-        player: '/players/sevilla.png'
+        player: '/players/sevilla.png',
+        team: '/backgrounds/sevilla.jpg'
     },
     valencia: {
         stadium:'./stadiums/valencia.jpg',
         logo:'/logos/valencia.png',
         font:'valencia',
         navColors:['#000000','#ffffff'],
-        player: '/players/valencia.png'
+        player: '/players/valencia.png',
+        team: '/backgrounds/valencia.jpg'
+
     },
     real: {
         stadium:'./stadiums/real.jpg',
         logo:'/logos/real.png',
         font:'barcelona',
         navColors:['#ffffff','#00529f'],
-        player: '/players/real.png'
+        player: '/players/real.png',
+        team: '/backgrounds/real.jpg'
+
     },
     barcelona: {
         stadium:'./stadiums/barcelona.jpg',
         logo:'/logos/barcelona.png',
         font:'barcelona',
         navColors:['#a50044','#004d98'],
-        player: '/players/barcelona.png'
+        player: '/players/barcelona.png',
+        team: '/backgrounds/barcelona.jpeg'
+
     },
     villarreal: {
         stadium:'./stadiums/villarreal.jpg',
         logo:'/logos/villarreal.png',
         font:'barcelona',
         navColors:['#ffe667','#005187'],
-        player: '/players/villarreal.png'
+        player: '/players/villarreal.png',
+        team: '/backgrounds/villarreal.jpg'
+
     },
     betis: {
         stadium:'./stadiums/betis.jpg',
         logo:'/logos/betis.png',
         font:'barcelona',
         navColors:['#0bb363','#ffffff'],
-        player: '/players/betis.png'
+        player: '/players/betis.png',
+        team: '/backgrounds/betis.jpg'
+
     },
     alaves: {
         stadium:'./stadiums/alaves.jpg',
         logo:'/logos/alaves.png',
         font:'barcelona',
         navColors:['#0761af','#009ad7'],
-        player: '/players/alaves.png'
+        player: '/players/alaves.png',
+        team: '/backgrounds/alaves.jpg'
+
     },
     atletico: {
         stadium:'./stadiums/atletico.jpg',
         logo:'/logos/atletico.png',
         font:'valencia',
         navColors:['#cb3524','#ffffff'],
-        player: '/players/atletico.png'
+        player: '/players/atletico.png',
+        team: '/backgrounds/atletico.jpg'
+
     },
     bilbao: {
         stadium:'./stadiums/bilbao.jpg',
         logo:'/logos/bilbao.png',
         font:'valencia',
         navColors:['#ee2523','#000000'],
-        player: '/players/bilbao.png'
+        player: '/players/bilbao.png',
+        team: '/backgrounds/bilbao.jpg'
+
     },
     celta: {
         stadium:'./stadiums/celta.jpg',
         logo:'/logos/celta.png',
         font:'barcelona',
         navColors:['#8ac3ee','#e5254e'],
-        player: '/players/celta.png'
+        player: '/players/celta.png',
+        team: '/backgrounds/celta.jpg'
+
     },
     eibar: {
         stadium:'./stadiums/eibar.jpg',
         logo:'/logos/eibar.png',
         font:'barcelona',
         navColors:['#0071b9','#aa093c'],
-        player: '/players/eibar.png'
+        player: '/players/eibar.png',
+        team: '/backgrounds/eibar.jpg'
+
     },
     espanol: {
         stadium:'./stadiums/espanol.jpg',
         logo:'/logos/espanol.png',
         font:'valencia',
         navColors:['#ffffff','#007fc8'],
-        player: '/players/espanol.png'
+        player: '/players/espanol.png',
+        team: '/backgrounds/espanol.jpg'
+
     },
     getafe: {
         stadium:'./stadiums/getafe.jpg',
         logo:'/logos/getafe.png',
         font:'barcelona',
         navColors:['#005999','#c43a27'],
-        player: '/players/getafe.png'
+        player: '/players/getafe.png',
+        team: '/backgrounds/getafe.jpg'
+
     },
     granada: {
         stadium:'./stadiums/granada.jpg',
         logo:'/logos/granada.png',
         font:'barcelona',
         navColors:['#ffffff','#a61b2b'],
-        player: '/players/granada.png'
+        player: '/players/granada.png',
+        team: '/backgrounds/granada.jpg'
+
     },
     leganes: {
         stadium:'./stadiums/leganes.jpg',
         logo:'/logos/leganes.png',
         font:'valencia',
         navColors:['#0c1f6e','#ffffff'],
-        player: '/players/leganes.png'
+        player: '/players/leganes.png',
+        team: '/backgrounds/leganes.jpg'
+
     },
     levante: {
         stadium:'./stadiums/levante.jpg',
         logo:'/logos/levante.png',
         font:'barcelona',
         navColors:['#b4053f','#005ca5'],
-        player: '/players/levante.png'
+        player: '/players/levante.png',
+        team: '/backgrounds/levante.jpg'
+
     },
     mallorca: {
         stadium:'./stadiums/mallorca.jpg',
         logo:'/logos/mallorca.png',
         font:'valencia',
         navColors:['#e20613','#000000'],
-        player: '/players/mallorca.png'
+        player: '/players/mallorca.png',
+        team: '/backgrounds/mallorca.jpg'
+
     },
     osasuna: {
         stadium:'./stadiums/osasuna.jpg',
         logo:'/logos/osasuna.png',
         font:'barcelona',
         navColors:['#0a346f','#d91a21'],
-        player: '/players/osasuna.png'
+        player: '/players/osasuna.png',
+        team: '/backgrounds/osasuna.jpg'
+
     },
     sociedad: {
         stadium:'./stadiums/sociedad.jpg',
         logo:'/logos/sociedad.png',
         font:'valencia',
         navColors:['#ffffff','#0067b1'],
-        player: '/players/sociedad.png'
+        player: '/players/sociedad.png',
+        team: '/backgrounds/sociedad.jpg'
+
     },
     valladolid: {
         stadium:'./stadiums/valladolid.jpg',
         logo:'/logos/valladolid.png',
         font:'barcelona',
         navColors:['#921b88','#ffffff'],
-        player: '/players/valladolid.png'
+        player: '/players/valladolid.png',
+        team: '/backgrounds/valladolid.jpg'
+
     }
 }
 let names = []
@@ -167,7 +206,7 @@ RIGHT_ARROW.addEventListener('click', ()=> {
     if(num === logos.length) num=0
     if(num<0) num=logos.length
     if(num !==0) document.querySelector(`.teamBox:nth-child(${num})`).style.border = ""
-  
+    document.querySelector('main').style.backgroundImage = `url(${clubs[names[num]].team})`
     document.querySelector(`.teamsBar`).style.background = `linear-gradient(0deg, ${clubs[names[num]].navColors[1]} 0%, ${clubs[names[num]].navColors[0]} 100%)`
     document.querySelector('.logoPng').style.setProperty('--club-logo', `url(${logos[num]})`)
     document.querySelector('.clubLogo').style.setProperty('--club-background-stadium', `url(${stadiums[num]})`)
@@ -187,7 +226,7 @@ LEFT_ARROW.addEventListener('click', ()=> {
     console.log(num)
     if(num === logos.length) num=0
     if(num<0) num=logos.length-1
-
+    document.querySelector('main').style.backgroundImage = `url(${clubs[names[num]].team})`
    document.querySelector(`.teamsBar`).style.background = `linear-gradient(0deg, ${clubs[names[num]].navColors[1]} 0%, ${clubs[names[num]].navColors[0]} 100%)`
    document.querySelector('.logoPng').style.setProperty('--club-logo', `url(${logos[num]})`)
     document.querySelector('.clubLogo').style.setProperty('--club-background-stadium', `url(${stadiums[num]})`)
@@ -204,6 +243,8 @@ document.querySelectorAll('.teamBox').forEach((elem, index) => elem.addEventList
     document.querySelectorAll('.teamBox').forEach(elem => {
         elem.style.border = "2px solid transparent"
     })
+    document.querySelector('main').style.backgroundImage = `url(${clubs[target].team})`
+
     document.querySelector(`.teamsBar`).style.background = `linear-gradient(0deg, ${clubs[target].navColors[1]} 0%, ${clubs[target].navColors[0]} 100%)`
     document.querySelector('.logoPng').style.setProperty('--club-logo', `url(${clubs[target].logo})`)
      document.querySelector('.clubLogo').style.setProperty('--club-background-stadium', `url(${clubs[target].stadium})`)
